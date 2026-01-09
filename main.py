1c43bfe78cdeae022b6810d9523d3b2ea314a425
Aaryan Anand Mudvikar <aaryanm1208@gmail.com>
2026-01-09 12:31:54 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+print("Program started")
+
+def main():
+    while True:
+        print("1. Test")
+        print("2. Exit")
+        choice = input("Choose: ")
+
+        if choice == "2":
+            break
+
+if __name__ == "__main__":
+    main()
